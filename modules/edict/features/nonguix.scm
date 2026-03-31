27cc21cb6e4b44994296e479d5dda8a587d6e7aa
hirancph <hiranchandph@gmail.com>
2026-04-01 00:10:07 +0530

    fix: ask substitute for guix first

--- modules/edict/features/nonguix.scm
+++ modules/edict/features/nonguix.scm
@@ -39,8 +39,8 @@ wire them in automatically.  System-scope only."
                      guix-service-type
                      (guix-extension
                       (substitute-urls
-                       (append '("https://substitutes.nonguix.org")
-                               %default-substitute-urls))
+                       (append %default-substitute-urls
+                               '("https://substitutes.nonguix.org")))
                       (authorized-keys
                        (append (list (plain-file "nonguix.pub"
                                       "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
