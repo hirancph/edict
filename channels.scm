ddc4b04073e99dc544e19fd634045ad33443ec69
hirancph <hiranchandph@gmail.com>
2026-03-31 12:59:22 +0530

    feat: guix channel change to codeberg

--- channels.scm
+++ channels.scm
@@ -10,12 +10,12 @@
  ;; Official GNU Guix channel
  (channel
   (name 'guix)
-  (url "https://git.savannah.gnu.org/git/guix.git")
+  (url "https://codeberg.org/guix/guix.git")
   (introduction
    (make-channel-introduction
     "9edb3f66fd807b096b48283debdcddccfea34bad"
     (openpgp-fingerprint
-     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+    "BBB0 2DFC 2603 4E8C AA15  7656 53E6 AF4F 1524 3DC2"))))
 
  ;; Nonguix — nonfree packages (linux kernel, NVIDIA drivers, etc.)
  (channel
