7bcbfe392ff5295f0fc7a827f2aa1d32a1a3c056
hirancph <hiranchandph@gmail.com>
2026-04-19 18:24:23 +0530

    fix: add missing fonts

---
+++ modules/edict/packages/fonts.scm
@@ -0,0 +1,66 @@
+;;; Copyright © 2026 hirancph
+;;;
+;;; packages/fonts.scm — Custom font packages for Caelestia.
+;;;
+;;; These are not (yet) in upstream Guix.  Adapted from nehrbash's
+;;; packages/fonts.scm.
+
+(define-module (edict packages fonts)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system font)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define-public font-rubik
+  (package
+    (name "font-rubik")
+    (version "0-git-e337a5f")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/googlefonts/rubik")
+             (commit "e337a5f69a9bea30e58d05bd40184d79cc099628")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0hg9611iiwzk8cff4ivfq7vbanks1q2xz68ijc5rq4cac05fg8cp"))))
+    (build-system font-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (add-before 'install 'chdir
+           (lambda _ (chdir "fonts/variable"))))))
+    (home-page "https://fonts.google.com/specimen/Rubik")
+    (synopsis "Sans-serif font family with slightly rounded corners")
+    (description "Rubik is a sans-serif font family with slightly rounded
+corners, designed by Hubert and Fischer, Meir Sadan, Cyreal.")
+    (license license:silofl1.1)))
+
+(define-public font-material-symbols-rounded
+  (package
+    (name "font-material-symbols-rounded")
+    (version "2024")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://github.com/google/material-design-icons/raw/master/"
+             "variablefont/MaterialSymbolsRounded%5BFILL%2CGRAD%2Copsz%2Cwght%5D.ttf"))
+       (file-name (string-append name "-" version ".ttf"))
+       (sha256
+        (base32 "0xx86154i43234nh467sjvx1bwpqfr0mm06y4zrz6lf01va2pzvb"))))
+    (build-system font-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (replace 'unpack
+           (lambda* (#:key source #:allow-other-keys)
+             (mkdir "source")
+             (copy-file source "source/MaterialSymbolsRounded.ttf")
+             (chdir "source"))))))
+    (home-page "https://fonts.google.com/icons")
+    (synopsis "Material Symbols Rounded icon font by Google")
+    (description "Variable icon font from Google's Material Symbols collection,
+using the Rounded style variant.")
+    (license license:asl2.0)))
